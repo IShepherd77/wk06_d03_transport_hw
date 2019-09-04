e04307e5763fc8705db1ce9d40b7d3896670685d
Ian Shepherd <ianshepherd77@gmail.com>
2019-09-04 19:52:22 +0100

    refactored code

--- models/traveller.js
+++ models/traveller.js
@@ -6,12 +6,6 @@ Traveller.prototype.getJourneyStartLocations = function() {
   return this.journeys.map(journey => journey.startLocation)
 };
 
-// Cinema.prototype.filmTitles = function () {
-//   return this.films.map((film) => {
-//     return film.title;
-//   });
-// };
-
 Traveller.prototype.getJourneyEndLocations = function () {
   return this.journeys.map(journey => journey.endLocation)
 };
@@ -22,24 +16,12 @@ Traveller.prototype.getJourneysByTransport = function (transport) {
   })
 };
 
-// Cinema.prototype.filmsByGenre = function (genre) {
-//   return this.films.filter((film) => {
-//     return film.genre === genre;
-//   });
-// };
-
 Traveller.prototype.getJourneysByMinDistance = function (minDistance) {
   return this.journeys.filter((journey) => {
     return journey.distance >= minDistance;
   })
-
 };
-//
-// Cinema.prototype.areAllFilmsOfMinLength = function (length) {
-//   return this.films.every((film) => {
-//     return film.length >= length;
-//   });
-// };
+
 
 Traveller.prototype.calculateTotalDistanceTravelled = function () {
   return this.journeys.reduce(function(totalDistance, journey) {
@@ -47,11 +29,6 @@ Traveller.prototype.calculateTotalDistanceTravelled = function () {
   },0)
 };
 
-// Cinema.prototype.totalRunningTime = function () {
-//   return this.films.reduce((total, film) => {
-//     return total += film.length;
-//   }, 0);
-// };
 
 Traveller.prototype.getUniqueModesOfTransport = function () {
   return this.journeys.find(function(transport) {
@@ -59,8 +36,5 @@ Traveller.prototype.getUniqueModesOfTransport = function () {
   })
 };
 
-// var found = array1.find(function(element) {
-//   return element > 10;
-// });
 
 module.exports = Traveller;
